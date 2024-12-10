b14126482c995257baad08c19aaee20a22f28679
Biky00 <167684107+Biky00@users.noreply.github.com>
2024-12-10 16:50:00 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // by writing the the old versions of JAVA  will treat as newer version.
+
+alert (3+3) // this can not run here because we are using node js not browser.
+
+console.log(3
+    + 3); // these kind of things we should not do because readability is needed.
+console.log( "vikram");
+
+
+let name = "vikram";
+let age = 20;
+let isLogedIn = false;
+
+
+// number   => 2 to the power 53
+// bigint
+// string  => ""
+// boolean => true/false
+//  null => standalone value 
+// undifined =>
+//  symbole => unique 
+
+// object 
+
+console.log(typeof undefined); // undifined 
+console.log(typeof null); // object
+
+
