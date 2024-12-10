5ed27d35f7a85ae2f3f08aaa06dd219d285bcc19
Biky00 <167684107+Biky00@users.noreply.github.com>
2024-12-10 16:48:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 124544;
+let accountEmail = "biky@gamil.com";
+var accountPasswored = "1234";
+accountCity = "Pakari";
+let accountState;
+
+// accountId = 2; //this is not allowed
+
+accountEmail = "bikiniki123@gmail.com";
+accountPasswored = "24547";
+accountCity = "Kandhamal";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPasswored, accountCity, accountState]);
+
+/* I should no use the var declaration word 
+because this give the problems in scop or constant scop */
